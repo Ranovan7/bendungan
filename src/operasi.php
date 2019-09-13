d6014714885bd9d6d75fdb4f555df3d07e570ecc
Ranovan7 <romyranovan@gmail.com>
2019-09-13 10:32:24 +0700

    added periodik daily module (not including TMA), change 'Dashboard' in navbar to 'Operasi'

--- src/operasi.php
+++ src/operasi.php
@@ -14,4 +14,73 @@ $app->group('/operasi', function() use ($loggedinMiddleware, $petugasAuthorizati
         return $this->view->render($response, 'admin.html');
     })->setName('operasi');
 
+    $this->group('/{id}', function() {
+
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            $hari = $request->getParam('sampling', date('Y-m-d'));
+            $id = $request->getAttribute('id');
+            $waduk = $this->db->query("SELECT * FROM waduk WHERE id={$id}")->fetch();
+
+            $month = $prev_date = date('m', strtotime($hari));
+            $year = $prev_date = date('Y', strtotime($hari));
+            $periodik = $this->db->query("SELECT * FROM periodik_daily
+                                            WHERE waduk_id={$id}
+                                                AND EXTRACT(MONTH FROM sampling)={$month}
+                                                AND EXTRACT(YEAR FROM sampling)={$year}")->fetchAll();
+
+            return $this->view->render($response, 'operasi/bendungan.html', [
+                'waduk' => $waduk,
+                'periodik' => $periodik,
+                'sampling' => $hari
+            ]);
+        })->setName('operasi.bendungan');
+
+        $this->group('/daily', function() {
+
+            $this->get('/add', function(Request $request, Response $response, $args) {
+                $hari = $request->getParam('sampling', date('Y-m-d'));
+                $id = $request->getAttribute('id');
+                $waduk = $this->db->query("SELECT * FROM waduk WHERE id={$id}")->fetch();
+
+                return $this->view->render($response, 'operasi/daily/add.html', [
+                    'waduk' => $waduk,
+                    'sampling' => $hari
+                ]);
+            })->setName('operasi.daily.add');
+
+            $this->post('/add', function(Request $request, Response $response, $args) {
+                $hari = date('Y-m-d');
+                $id = $request->getAttribute('id');
+                $waduk = $this->db->query("SELECT * FROM waduk WHERE id={$id}")->fetch();
+
+                $form = $request->getParams();
+
+                $stmt = $this->db->prepare("INSERT INTO periodik_daily
+                                        (sampling, curahhujan, inflow_deb, inflow_vol,
+                                            outflow_deb, outflow_vol, spillway_deb, spillway_vol,
+                                            waduk_id)
+                                        VALUES
+                                        (:sampling, :curahhujan, :inflow_deb, :inflow_vol,
+                                            :outflow_deb, :outflow_vol, :spillway_deb, :spillway_vol,
+                                            :waduk_id)");
+                $stmt->execute([
+                    ':sampling' => $form['sampling'],
+                    ':curahhujan' => $form['ch'],
+                    ':inflow_deb' => $form['debit'],
+                    ':inflow_vol' => $form['volume'],
+                    ':outflow_deb' => $form['deb-in'],
+                    ':outflow_vol' => $form['vol-in'],
+                    ':spillway_deb' => $form['deb-spill'],
+                    ':spillway_vol' => $form['vol-spill'],
+                    ':waduk_id' => $id,
+                ]);
+
+                $this->flash->addMessage('messages', 'Periodik Daily berhasil ditambahkan');
+                return $this->response->withRedirect($this->router->pathFor('operasi.bendungan', ['id' => $id], []));
+            })->setName('operasi.daily.add');
+
+        });
+
+    })->add($petugasAuthorizationMiddleware);
+
 })->add($loggedinMiddleware);
